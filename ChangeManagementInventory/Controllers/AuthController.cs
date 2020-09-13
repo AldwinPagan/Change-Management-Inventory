21dbe5eff85180caa60112eb55b19caccd2b304c
AldwinPagan <aldwin.pagan@gmail.com>
2020-09-13 14:26:54 -0400

    Initial Commit

---
+++ ChangeManagementInventory/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChangeManagementInventory.Controllers
+{
+    // /Auth
+    public class AuthController : Controller
+    {
+        // /Auth/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
